bf97a59fa67863fe00de64f5794f0632fafdb014
Jawad Raza <jawadraza8372@gmail.com>
2022-05-14 01:57:33 -0700

    final changes

--- src/Screens/OrderDetailScreen/OrderDetailScreen.js
+++ src/Screens/OrderDetailScreen/OrderDetailScreen.js
@@ -8,297 +8,295 @@ import moment from "moment";
 import { jsPDF } from "jspdf";
 
 function OrderDetailScreen() {
-  const { id } = useParams();
-  const { isLoading, setisLoading } = useLoadingWithRefreash();
+	const { id } = useParams();
+	const { isLoading, setisLoading } = useLoadingWithRefreash();
 
-  const { orders } = useSelector((state) => state.project);
-  useEffect(() => {
-    if (orders.length <= 0) {
-      setisLoading(true);
-    }
-  }, [orders, setisLoading]);
+	const { orders } = useSelector((state) => state.project);
+	useEffect(() => {
+		if (orders.length <= 0) {
+			setisLoading(true);
+		}
+	}, [orders, setisLoading]);
 
-  const data = orders.filter((dat) => dat._id === id);
-  const {
-    ClientName,
-    ClientNumber,
-    ClientIdentityNumber,
-    ClientEmail,
-    ClientFrontIDcard,
-    ClientBackIDcard,
-    CarName,
-    CarModel,
-    CarPirateNumber,
-    CarCurrentFuel,
-    CarCurrentKMS,
-    CarFrontSideImage,
-    CarLeftSideImage,
-    CarRightSideImage,
-    CarBackSideImage,
-    CarNumberPlateImage,
-    CarFrontLeftTypeImage,
-    CarFrontRightTypeImage,
-    CarBackRightTypeImage,
-    CarBackLeftTypeImage,
-    DeliveryTime,
-    DeliveryDate,
-    EstimatedDeliveryDateandTime,
-    Signature,
-    EmployeeEmail,
-    EmployeeName,
-    Arrival,
-  } = data[0];
-  const downloadpdfinfo = () => {
-    const doc = new jsPDF();
-    doc.text(`Client Name: ${ClientName}`, 25, 25);
-    doc.text(`Client Phone Number: ${ClientNumber}`, 25, 35);
-    doc.text(`Client CNIC: ${ClientIdentityNumber}`, 25, 45);
-    doc.text(`Client Emial: ${ClientEmail}`, 25, 55);
-    doc.text(`Car Name: ${CarName}`, 25, 65);
-    doc.text(`Car Model: ${CarModel}`, 25, 75);
-    doc.text(`Car Pirate Number: ${CarPirateNumber}`, 25, 85);
-    doc.text(`Car Current Fuel: ${CarCurrentFuel}`, 25, 95);
-    doc.text(`Car Current KMS: ${CarCurrentKMS}`, 25, 105);
-    doc.text(`Delivery Time: ${DeliveryTime}`, 25, 115);
-    doc.text(`Delivery Date: ${DeliveryDate}`, 25, 125);
-    doc.text(
-      `Estimated Delivery Date and Time: ${EstimatedDeliveryDateandTime}`,
-      25,
-      135
-    );
-    doc.text(`Employee Email: ${EmployeeEmail}`, 25, 145);
-    doc.text(`Employee Name: ${EmployeeName}`, 25, 155);
-    doc.text(`Arrival :${Arrival}`, 25, 165);
+	const data = orders.filter((dat) => dat._id === id);
+	const {
+		ClientName,
+		ClientNumber,
+		ClientIdentityNumber,
+		ClientEmail,
+		ClientFrontIDcard,
+		ClientBackIDcard,
+		CarName,
+		CarModel,
+		CarPirateNumber,
+		CarCurrentFuel,
+		CarCurrentKMS,
+		CarFrontSideImage,
+		CarLeftSideImage,
+		CarRightSideImage,
+		CarBackSideImage,
+		CarNumberPlateImage,
+		CarFrontLeftTypeImage,
+		CarFrontRightTypeImage,
+		CarBackRightTypeImage,
+		CarBackLeftTypeImage,
+		DeliveryTime,
+		DeliveryDate,
+		EstimatedDeliveryDateandTime,
+		Signature,
+		EmployeeEmail,
+		EmployeeName,
+		Arrival,
+	} = data[0];
+	const downloadpdfinfo = () => {
+		const doc = new jsPDF();
+		doc.text(`Client Name: ${ClientName}`, 25, 25);
+		doc.text(`Client Phone Number: ${ClientNumber}`, 25, 35);
+		doc.text(`Client CNIC: ${ClientIdentityNumber}`, 25, 45);
+		doc.text(`Client Emial: ${ClientEmail}`, 25, 55);
+		doc.text(`Car Name: ${CarName}`, 25, 65);
+		doc.text(`Car Model: ${CarModel}`, 25, 75);
+		doc.text(`Car Pirate Number: ${CarPirateNumber}`, 25, 85);
+		doc.text(`Car Current Fuel: ${CarCurrentFuel}`, 25, 95);
+		doc.text(`Car Current KMS: ${CarCurrentKMS}`, 25, 105);
+		doc.text(`Delivery Time: ${DeliveryTime}`, 25, 115);
+		doc.text(`Delivery Date: ${DeliveryDate}`, 25, 125);
+		doc.text(
+			`Estimated Delivery Date and Time: ${EstimatedDeliveryDateandTime}`,
+			25,
+			135
+		);
+		doc.text(`Employee Email: ${EmployeeEmail}`, 25, 145);
+		doc.text(`Employee Name: ${EmployeeName}`, 25, 155);
+		doc.text(`Arrival :${Arrival}`, 25, 165);
 
-    doc.save(`order_${id}_info`);
-  };
-  if (isLoading) {
-    return <Loader />;
-  }
+		doc.save(`order_${id}_info`);
+	};
+	if (isLoading) {
+		return <Loader />;
+	}
 
-  return (
-    <div className="orderdeltscreen">
-      <div className="ordernumber">
-        <h1>Order Details</h1>
-        <span>{id}</span>
-      </div>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "row",
-        }}
-      >
-        <button
-          style={{
-            width: "100%",
-            maxWidth: "150px",
-            height: "60px",
-            color: "#022c43",
-            background: "#fff61b",
-            fontSize: "20px",
-            fontWeight: "bold",
-            margin: "10px 0",
-          }}
-          onClick={downloadpdfinfo}
-        >
-          downlod pdf
-        </button>
-      </div>
-      {ClientName && (
-        <p>
-          <span>Client Name:</span>
-          {ClientName ? ClientName : "Not Available"}
-        </p>
-      )}
-      {ClientNumber && (
-        <p>
-          <span>Client Phone:</span>
-          {ClientNumber ? ClientNumber : "Not Available"}
-        </p>
-      )}
-      {ClientIdentityNumber && (
-        <p>
-          <span>Client CNIC:</span>
-          {ClientIdentityNumber ? ClientIdentityNumber : "Not Available"}
-        </p>
-      )}
-      {ClientEmail && (
-        <p>
-          <span>Client Email:</span>
-          {ClientEmail ? ClientEmail : "Not Available"}
-        </p>
-      )}
-      {ClientFrontIDcard && (
-        <>
-          <p>
-            <span>Client Front side CNIC:</span>
-          </p>
-          <img src={`${ClientFrontIDcard}`} alt="Client Front side CNIC" />
-        </>
-      )}
-      {ClientBackIDcard && (
-        <>
-          <p>
-            <span>Client Back side CNIC:</span>
-          </p>
-          <img src={`${ClientBackIDcard}`} alt="Client Back side CNIC" />
-        </>
-      )}
-      {CarName && (
-        <p>
-          <span>Car Name:</span>
-          {CarName ? CarName : "Not Available"}
-        </p>
-      )}
+	return (
+		<div className='orderdeltscreen'>
+			<div className='ordernumber'>
+				<h1>Order Details</h1>
+				<span>{id}</span>
+			</div>
+			<div
+				style={{
+					width: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					flexDirection: "row",
+				}}>
+				<button
+					style={{
+						width: "100%",
+						maxWidth: "150px",
+						height: "60px",
+						color: "#282a33",
+						background: "#e94c3d",
+						fontSize: "20px",
+						fontWeight: "bold",
+						margin: "10px 0",
+					}}
+					onClick={downloadpdfinfo}>
+					downlod pdf
+				</button>
+			</div>
+			{ClientName && (
+				<p>
+					<span>Client Name:</span>
+					{ClientName ? ClientName : "Not Available"}
+				</p>
+			)}
+			{ClientNumber && (
+				<p>
+					<span>Client Phone:</span>
+					{ClientNumber ? ClientNumber : "Not Available"}
+				</p>
+			)}
+			{ClientIdentityNumber && (
+				<p>
+					<span>Client CNIC:</span>
+					{ClientIdentityNumber ? ClientIdentityNumber : "Not Available"}
+				</p>
+			)}
+			{ClientEmail && (
+				<p>
+					<span>Client Email:</span>
+					{ClientEmail ? ClientEmail : "Not Available"}
+				</p>
+			)}
+			{ClientFrontIDcard && (
+				<>
+					<p>
+						<span>Client Front side CNIC:</span>
+					</p>
+					<img src={`${ClientFrontIDcard}`} alt='Client Front side CNIC' />
+				</>
+			)}
+			{ClientBackIDcard && (
+				<>
+					<p>
+						<span>Client Back side CNIC:</span>
+					</p>
+					<img src={`${ClientBackIDcard}`} alt='Client Back side CNIC' />
+				</>
+			)}
+			{CarName && (
+				<p>
+					<span>Car Name:</span>
+					{CarName ? CarName : "Not Available"}
+				</p>
+			)}
 
-      {CarModel && (
-        <p>
-          <span>Car Model:</span>
-          {CarModel ? CarModel : "Not Available"}
-        </p>
-      )}
-      {CarPirateNumber && (
-        <p>
-          <span>Car Pirate Number:</span>
-          {CarPirateNumber ? CarPirateNumber : "Not Available"}
-        </p>
-      )}
-      {CarCurrentFuel && (
-        <p>
-          <span>Car Current Fuel:</span>
-          {CarCurrentFuel ? CarCurrentFuel : "Not Available"}
-        </p>
-      )}
-      {CarCurrentKMS && (
-        <p>
-          <span>Car Current KMS:</span>
-          {CarCurrentKMS ? CarCurrentKMS : "Not Available"}
-        </p>
-      )}
-      {CarFrontSideImage && (
-        <>
-          <p>
-            <span>Car Front Side Image:</span>
-          </p>
-          <img src={`${CarFrontSideImage}`} alt="Car Front Side " />
-        </>
-      )}
-      {CarLeftSideImage && (
-        <>
-          <p>
-            <span>Car Left Side Image:</span>
-          </p>
-          <img src={`${CarLeftSideImage}`} alt="Car Left Side " />
-        </>
-      )}
-      {CarRightSideImage && (
-        <>
-          <p>
-            <span>Car Right Side Image:</span>
-          </p>
-          <img src={`${CarRightSideImage}`} alt="Car Right Side " />
-        </>
-      )}
+			{CarModel && (
+				<p>
+					<span>Car Model:</span>
+					{CarModel ? CarModel : "Not Available"}
+				</p>
+			)}
+			{CarPirateNumber && (
+				<p>
+					<span>Car Pirate Number:</span>
+					{CarPirateNumber ? CarPirateNumber : "Not Available"}
+				</p>
+			)}
+			{CarCurrentFuel && (
+				<p>
+					<span>Car Current Fuel:</span>
+					{CarCurrentFuel ? CarCurrentFuel : "Not Available"}
+				</p>
+			)}
+			{CarCurrentKMS && (
+				<p>
+					<span>Car Current KMS:</span>
+					{CarCurrentKMS ? CarCurrentKMS : "Not Available"}
+				</p>
+			)}
+			{CarFrontSideImage && (
+				<>
+					<p>
+						<span>Car Front Side Image:</span>
+					</p>
+					<img src={`${CarFrontSideImage}`} alt='Car Front Side ' />
+				</>
+			)}
+			{CarLeftSideImage && (
+				<>
+					<p>
+						<span>Car Left Side Image:</span>
+					</p>
+					<img src={`${CarLeftSideImage}`} alt='Car Left Side ' />
+				</>
+			)}
+			{CarRightSideImage && (
+				<>
+					<p>
+						<span>Car Right Side Image:</span>
+					</p>
+					<img src={`${CarRightSideImage}`} alt='Car Right Side ' />
+				</>
+			)}
 
-      {CarRightSideImage && (
-        <>
-          <p>
-            <span>Car Right Side Image:</span>
-          </p>
-          <img src={`${CarRightSideImage}`} alt="Car Right Side " />
-        </>
-      )}
-      {CarBackSideImage && (
-        <>
-          <p>
-            <span>Car Back Side Image:</span>
-          </p>
-          <img src={`${CarBackSideImage}`} alt="Car Back Side " />
-        </>
-      )}
-      {CarNumberPlateImage && (
-        <>
-          <p>
-            <span>Car Number Plate Image:</span>
-          </p>
-          <img src={`${CarNumberPlateImage}`} alt="Car Number Plate " />
-        </>
-      )}
-      {CarFrontLeftTypeImage && (
-        <>
-          <p>
-            <span>Car Front Left Type Image:</span>
-          </p>
-          <img src={`${CarFrontLeftTypeImage}`} alt="Car Front Left Type " />
-        </>
-      )}
-      {CarFrontRightTypeImage && (
-        <>
-          <p>
-            <span>Car Front Right Type Image:</span>
-          </p>
-          <img src={`${CarFrontRightTypeImage}`} alt="Car Front Right Type" />
-        </>
-      )}
-      {CarBackRightTypeImage && (
-        <>
-          <p>
-            <span>Car Back Right Type Image:</span>
-          </p>
-          <img src={`${CarBackRightTypeImage}`} alt="Car Back Right Type" />
-        </>
-      )}
-      {CarBackLeftTypeImage && (
-        <>
-          <p>
-            <span>Car Back Left Type Image:</span>
-          </p>
-          <img src={`${CarBackLeftTypeImage}`} alt="Car Back Left Type" />
-        </>
-      )}
+			{CarRightSideImage && (
+				<>
+					<p>
+						<span>Car Right Side Image:</span>
+					</p>
+					<img src={`${CarRightSideImage}`} alt='Car Right Side ' />
+				</>
+			)}
+			{CarBackSideImage && (
+				<>
+					<p>
+						<span>Car Back Side Image:</span>
+					</p>
+					<img src={`${CarBackSideImage}`} alt='Car Back Side ' />
+				</>
+			)}
+			{CarNumberPlateImage && (
+				<>
+					<p>
+						<span>Car Number Plate Image:</span>
+					</p>
+					<img src={`${CarNumberPlateImage}`} alt='Car Number Plate ' />
+				</>
+			)}
+			{CarFrontLeftTypeImage && (
+				<>
+					<p>
+						<span>Car Front Left Type Image:</span>
+					</p>
+					<img src={`${CarFrontLeftTypeImage}`} alt='Car Front Left Type ' />
+				</>
+			)}
+			{CarFrontRightTypeImage && (
+				<>
+					<p>
+						<span>Car Front Right Type Image:</span>
+					</p>
+					<img src={`${CarFrontRightTypeImage}`} alt='Car Front Right Type' />
+				</>
+			)}
+			{CarBackRightTypeImage && (
+				<>
+					<p>
+						<span>Car Back Right Type Image:</span>
+					</p>
+					<img src={`${CarBackRightTypeImage}`} alt='Car Back Right Type' />
+				</>
+			)}
+			{CarBackLeftTypeImage && (
+				<>
+					<p>
+						<span>Car Back Left Type Image:</span>
+					</p>
+					<img src={`${CarBackLeftTypeImage}`} alt='Car Back Left Type' />
+				</>
+			)}
 
-      {DeliveryTime ||
-        (DeliveryDate && (
-          <p>
-            <span>Delivery :</span>
-            {DeliveryTime} {DeliveryDate}
-          </p>
-        ))}
-      {EstimatedDeliveryDateandTime && (
-        <p>
-          <span>Estimated Delivery Date and Time:</span>
-          {moment(EstimatedDeliveryDateandTime).format("D MMM , h:mm a")}
-        </p>
-      )}
-      {Signature && (
-        <>
-          <p>
-            <span>Signature:</span>
-          </p>
-          <img src={`${Signature}`} alt="Signature" />
-        </>
-      )}
-      {EmployeeEmail && (
-        <p>
-          <span>Employe Email:</span>
-          {EmployeeEmail ? EmployeeEmail : "Not Available"}
-        </p>
-      )}
-      {EmployeeName && (
-        <p>
-          <span>Employe Name:</span>
-          {EmployeeName ? EmployeeName : "Not Available"}
-        </p>
-      )}
-      <p>
-        <span>Completed:</span>
-        {Arrival ? "True" : "False"}
-      </p>
-    </div>
-  );
+			{DeliveryTime ||
+				(DeliveryDate && (
+					<p>
+						<span>Delivery :</span>
+						{DeliveryTime} {DeliveryDate}
+					</p>
+				))}
+			{EstimatedDeliveryDateandTime && (
+				<p>
+					<span>Estimated Delivery Date and Time:</span>
+					{moment(EstimatedDeliveryDateandTime).format("D MMM , h:mm a")}
+				</p>
+			)}
+			{Signature && (
+				<>
+					<p>
+						<span>Signature:</span>
+					</p>
+					<img src={`${Signature}`} alt='Signature' />
+				</>
+			)}
+			{EmployeeEmail && (
+				<p>
+					<span>Employe Email:</span>
+					{EmployeeEmail ? EmployeeEmail : "Not Available"}
+				</p>
+			)}
+			{EmployeeName && (
+				<p>
+					<span>Employe Name:</span>
+					{EmployeeName ? EmployeeName : "Not Available"}
+				</p>
+			)}
+			<p>
+				<span>Completed:</span>
+				{Arrival ? "True" : "False"}
+			</p>
+		</div>
+	);
 }
 
 export default OrderDetailScreen;
